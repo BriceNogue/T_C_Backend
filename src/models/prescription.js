abaf2722cff4e590aba7a8dc6a18636511d026fa
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2023-04-23 15:35:41 +0200

    First commit

---
+++ src/models/prescription.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const prescriptionModel = mongoose.Schema({
+    prescription_code:{
+        type: String,
+        require: true
+    },
+    prescription_date:{
+        type: String,
+        require: true,
+    },
+    
+});
+
+module.exports = mongoose.model('Prescritions', prescriptionModel);
